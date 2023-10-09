a53345f95cdcfb6f2c29c5434dd4a6a15557118f
GDDMN <apawkin@gmail.com>
2023-10-09 17:44:26 +0300

    fixrs

--- Assets/Scripts/Gameplay/Cube.cs
+++ Assets/Scripts/Gameplay/Cube.cs
@@ -4,34 +4,31 @@ using UnityEngine;
 using TMPro;
 using System.Collections;
 
-[Serializable]
-public struct CubeData
-{
-  public int Level;
-  public int ColorNum;
-}
-
-
 public class Cube : MonoBehaviour
 {
   public event Action<Cube, Cube> OnMerge;
 
+  [Header("Jumping")]
+  [SerializeField] private AnimationCurve _jumpCurve;
+  [SerializeField] private float _jumpSpeed;
+  [SerializeField] private float _jumpForce;
+
+  [Header("Visual")]
+  [SerializeField] private ParticleSystem _mergeEffect;
   [SerializeField] private List<TextMeshPro> _allNumbers;
   [SerializeField] private List<Color> _allColors;
 
-  [SerializeField] private AnimationCurve _jumpCurve;
-  [SerializeField] private float _jumpSpeed;
-
-  [SerializeField] private ParticleSystem _mergeEffect;
-
+  [Header("Data")]
   public CubeData Data;
+
   public bool AlreadyMerging = false;
 
   private Rigidbody _rigidbody;
   private Renderer _renderer;
   private bool _flying = false;
   private float _yJumpPos = 0f;
-  Vector3 _position;
+  private Vector3 _position;
+
   private void Awake()
   {
     _renderer = gameObject.GetComponent<Renderer>();
@@ -93,64 +90,80 @@ public class Cube : MonoBehaviour
     List<Cube> unLevelCubes = allCubes.FindAll(c => c.Data.Level == Data.Level && c != this);
 
     Cube nextMergeCube = FindNearCube(unLevelCubes);
-
-    if (nextMergeCube != null 
-        && nextMergeCube.gameObject.GetComponent<CubeSwipeHandler>() == null
-        && Math.Abs(nextMergeCube.transform.position.x - transform.position.x) < 3.5f
-        && Math.Abs(nextMergeCube.transform.position.z - transform.position.z) < 3.5f)
-      _position = nextMergeCube.transform.position;
-    else
-      _position = transform.position;
-
+    _position = ValidateCubeJumpPos(nextMergeCube);
     JumpToOtherCube(_position);
   }
 
   private Cube FindNearCube(List<Cube> allCubes)
-  {
-
+  { 
     Cube nearCube;
 
     if (allCubes.Count <= 0)
       return null;
 
     nearCube = allCubes[0];
+    Transform nearCubePos = nearCube.transform;
+
 
     foreach (var cube in allCubes)
     {
-      if (Math.Abs(transform.position.x - cube.transform.position.x) < Math.Abs(transform.position.x - nearCube.transform.position.x) &&
-          Math.Abs(transform.position.z - cube.transform.position.z) < Math.Abs(transform.position.z - nearCube.transform.position.z))
+      float nearCubeDistance = Vector3.Distance(transform.position, nearCubePos.position);
+      float newCubeDistance = Vector3.Distance(transform.position, cube.transform.position);
+
+      if (nearCubeDistance > newCubeDistance)
+      {
         nearCube = cube;
+        nearCubePos = nearCube.transform;
+      }
     }
 
     return nearCube;
   }
 
+  private Vector3 ValidateCubeJumpPos(Cube nextMergeCube)
+  {
+    Vector3 position;
+
+    if (nextMergeCube != null && nextMergeCube.gameObject.GetComponent<CubeSwipeHandler>() == null)
+      position = nextMergeCube.transform.position;
+    else
+      position = transform.position;
+
+    return position;
+  }
+
   private void JumpToOtherCube(Vector3 position)
   {
-    if (!_flying)
+    if (_flying)
       return;
 
-    _flying = false;
+    _yJumpPos = 0f;
+    _flying = true;
+    _rigidbody.useGravity = false;
     StartCoroutine(JumpAnimation());
   }
 
   private IEnumerator JumpAnimation()
   {
-    while(!_flying && _yJumpPos <= 1f)
+    float yPos = transform.position.y;
+
+    while (_flying && _yJumpPos <= 1f)
     {
+      _yJumpPos += _jumpSpeed * Time.deltaTime;
+      
       transform.position = new Vector3(transform.position.x,
-                                 _jumpCurve.Evaluate(_yJumpPos),
+                                 yPos + (_jumpCurve.Evaluate(_yJumpPos) * _jumpForce),
                                  transform.position.z);
 
       transform.position += new Vector3((_position.x - transform.position.x) * _yJumpPos / 10,
                                         0f,
                                         (_position.z - transform.position.z) * _yJumpPos / 10);
+      Debug.Log(yPos + (_jumpCurve.Evaluate(_yJumpPos) * _jumpForce));
 
-      _yJumpPos += _jumpSpeed * Time.deltaTime;
       yield return null;
     }
 
-    _flying = true;
+    _flying = false;
     _yJumpPos = 0f;
   }
 
@@ -158,15 +171,6 @@ public class Cube : MonoBehaviour
   {
     _rigidbody.useGravity = true;
 
-    if (collision.gameObject.layer == 10)
-    {
-      _flying = true;
-      Merging(collision);
-    }
-  }
-
-  private void OnCollisionStay(Collision collision)
-  {
     if (collision.gameObject.layer == 10)
     {
       Merging(collision);
